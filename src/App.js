52b1a7773da9b7a661ace2355e520176b0f6e9a1
Camila Fenzl <camilafenzl@Camilas-MacBook-Pro.local>
2023-05-05 10:27:24 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import randomColor from 'randomcolor';
 import React,{ useState } from 'react';
 
 export default function App(){
-  const [color, setColor] = useState(randomColor);
-  const [inputColor, setInputColor] = useState();
+  const [color1, setColor1] = useState(randomColor());
+  const [color2, setColor2] = useState(randomColor());
+  function generate(){
+    setColor1(randomColor());
+    setColor2(randomColor());
+  }
   return (
-    <div className="App">
-    <div>
-    <button onClick={() => setColor(randomColor())}>Generate</button>
-    </div>
+    <div className="App" style={{backgroundColor: color1}} >
+      <div style={{backgroundColor: color2}} >
+        <button onClick={() => generate()}>Generate</button>
+        <div>Generated Color: {color2}</div>
+      </div>
+      <div>Generated Color: {color1}</div>
     </div>
     );
 }
