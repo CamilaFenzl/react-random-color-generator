2377cc43189438070243204763e153eedf1f1100
Camila Fenzl <camilafenzl@gmail.com>
2023-05-05 13:04:38 +0200

    Last changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import './App.css';
 import randomColor from 'randomcolor';
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 
-export default function App(){
+export default function App() {
   const [color1, setColor1] = useState(randomColor());
   const [color2, setColor2] = useState(randomColor());
-  function generate(){
+  function generate() {
     setColor1(randomColor());
     setColor2(randomColor());
   }
   return (
-    <div className="App" style={{backgroundColor: color1}} >
-      <div style={{backgroundColor: color2}} >
+    <div className="App" style={{ backgroundColor: color1 }}>
+      <div style={{ backgroundColor: color2 }}>
         <button onClick={() => generate()}>Generate</button>
         <div>Generated Color: {color2}</div>
       </div>
       <div>Generated Color: {color1}</div>
     </div>
-    );
+  );
 }
